023439686d393439c95e5149fd1c46b40efa28a9
satrut <satrut@qq.com>
2023-04-13 23:48:34 +0800

    。

--- generate/visual.py
+++ generate/visual.py
@@ -10,16 +10,20 @@ parser.add_argument('-f', required=True, type=str,
                     help='the file name for the graph')
 parser.add_argument('-o', required=True, type=str,
                     help='name of visualization image')
+parser.add_argument('--undirected', action='store_true',
+                    help='display an undirected graph instead of the default directed one')
 
 opt = parser.parse_args()
 opt.o = os.path.basename(opt.o)
 
-Graph = nx.Graph()
+Graph = nx.Graph() if opt.undirected else nx.DiGraph()
 Graph.add_nodes_from(range(opt.v))
 with open(opt.f, "rt") as f:
     for line in f.readlines():
         i, j, w = line.split()
         i, j, w = int(i), int(j), int(w)
         Graph.add_edge(i, j, weight=w)
-nx.draw(Graph, with_labels=True)
+# pos = nx.circular_layout(Graph)
+nx.draw_shell(Graph, with_labels=True)
+
 plt.savefig(opt.o)
